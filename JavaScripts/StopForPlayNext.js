9dfb89252f79bcb6e8063e7f566bb2973537398f
Oruc Qafarov <orr888@gmail.com>
2021-12-16 14:59:45 +0400

    a

--- JavaScripts/StopForPlayNext.js
+++
@@ -1,2 +0,0 @@
-// NEXT SOUND PLAY WITH STOP CURRENT SOUND SCRIPT //
-document.addEventListener("play", function (e) { for (var t = document.getElementsByTagName("audio"), a = 0, n = t.length; a < n; a++) t[a] != e.target && t[a].pause() }, !0)
